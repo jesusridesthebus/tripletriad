76e5a92c409b068af2b291f5f455f4d16d1dc93c
Student <example@example.com>
2019-07-23 14:10:00 -0700

    add board ondrop

--- ClientApp/src/components/Board.js
+++ ClientApp/src/components/Board.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import './Board.css';
+import PropTypes from 'prop-types';
 
-export default function Board() {
+export default function Board(props) {
+
+  function dropCard(position) {
+    props.dropCard(position);
+  }
+
   return(
     <div className='board'>
-      <div className='boardItem'></div>
-      <div className='boardItem'></div>
-      <div className='boardItem'></div>
-      <div className='boardItem'></div>
-      <div className='boardItem'></div>
-      <div className='boardItem'></div>
-      <div className='boardItem'></div>
-      <div className='boardItem'></div>
-      <div className='boardItem'></div>
+      <div onDragOver={() => dropCard(0)} className='boardItem a0'></div>
+      <div onDragOver={() => dropCard(1)} className='boardItem a1'></div>
+      <div onDragOver={() => dropCard(2)} className='boardItem a2'></div>
+      <div onDragOver={() => dropCard(3)} className='boardItem a3'></div>
+      <div onDragOver={() => dropCard(4)} className='boardItem a4'></div>
+      <div onDragOver={() => dropCard(5)} className='boardItem a5'></div>
+      <div onDragOver={() => dropCard(6)} className='boardItem a6'></div>
+      <div onDragOver={() => dropCard(7)} className='boardItem a7'></div>
+      <div onDragOver={() => dropCard(8)} className='boardItem a8'></div>
     </div>
   );
+}
+
+Board.propTypes = {
+  dropCard: PropTypes.func
 }
